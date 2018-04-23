8a51e6e19a50e638cc0d1b57cbcf8e4f94bfc303
Hackeru_Student <hackeru@hackeru-students-mac.local>
2018-04-23 01:40:24 -0700

    Initial Commit

---
+++ MyDuaAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyDuaAnimation
+//
+//  Created by Hackeru_Student on 4/23/18.
+//  Copyright © 2018 Leena Erhayel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
